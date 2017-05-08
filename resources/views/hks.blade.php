12732823c9833bc7a6c41b95ffc48d061848a915
Boris Juric <[boris.juric@altima.hr]>
2017-05-08 15:10:13 +0200

    templates

---
+++ resources/views/hks.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="container-fluid" style="min-height: 100%; height: 100%;">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-9">
+
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html ng-app="crm">
+<head>
+    <title>BJ</title>
+
+    <link rel="stylesheet" href={{asset("css/app.css")}}>
+
+    @section('style')
+        {{-- Add additional style sheets here --}}
+    @show
+
+</head>
+<body ng-cloak>
+
+@section('content')
+    {{-- Add content here --}}
+@show
+
+
+<script src="{{ asset("jspm_packages/system.js") }}"></script>
+<script src="{{ asset("config.js") }}"></script>
+<script>
+    System
+        .import('{{ asset('js/main') }}')
+        .catch(console.error.bind(console));
+</script>
+
+@section('javascript')
+    {{-- Add additional javascript blocks here --}}
+@show
+
+</body>
+
+</html>
